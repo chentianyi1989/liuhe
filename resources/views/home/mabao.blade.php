34da23136e22d7a1073e1543dc372fffb0f75e8e
448987985@qq.com <448987985@qq.com>
2017-11-23 06:58:07 +0800

    11

--- resources/views/home/mabao.blade.php
+++ resources/views/home/mabao.blade.php
@@ -8,11 +8,11 @@
 
 @section('content')
 
-<div class="L_HK6 P_tm skin_red lhc_center" style="width: 1300px">
+<div  style=";margin: 10px auto 0 auto;">
 
-	<img alt="" src="{{asset('/mabao/1.jpg')}}" style="width: 400px;padding-left: 30px;">	
-	<img alt="" src="{{asset('/mabao/2.jpg')}}" style="width: 400px;padding-left: 30px;">
-	<img alt="" src="{{asset('/mabao/3.jpg')}}" style="width: 400px;padding-left: 30px;">
+	<img alt="" src="{{asset('/mabao/1.jpg')}}" style="width: 600px;padding-left: 30px;">	
+	<img alt="" src="{{asset('/mabao/2.jpg')}}" style="width: 600px;padding-left: 30px;">
+	<img alt="" src="{{asset('/mabao/3.jpg')}}" style="width: 600px;padding-left: 30px;">
 	
 </div>	
 @endsection
