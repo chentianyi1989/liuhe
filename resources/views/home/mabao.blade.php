cbf1e0e5a13846e97039108199db0c3dd971cf73
448987985@qq.com <448987985@qq.com>
2017-11-29 22:39:51 +0800

    1

--- resources/views/home/mabao.blade.php
+++ resources/views/home/mabao.blade.php
@@ -8,12 +8,12 @@
 
 @section('content')
 
-<div  style=";margin: 10px auto 0 auto;">
+<div style="margin: 10px auto 0 auto;">
 
-	<img alt="" src="{{asset('/mabao/1.jpg')}}" style="width: 600px;padding-left: 30px;">	
-	<img alt="" src="{{asset('/mabao/2.jpg')}}" style="width: 600px;padding-left: 30px;">
-	<img alt="" src="{{asset('/mabao/3.jpg')}}" style="width: 600px;padding-left: 30px;">
-	
+	<img alt="" src="{{asset('/mabao/a1.jpg')}}" style="width: 49%;padding-left: 30px;">	
+	<img alt="" src="{{asset('/mabao/a2.jpg')}}" style="width: 49%;padding-left: 30px;">
+	<img alt="" src="{{asset('/mabao/a3.jpg')}}" style="width: 49%;padding-left: 30px;">
+	<img alt="" src="{{asset('/mabao/a4.jpg')}}" style="width: 49%;padding-left: 30px;">
 </div>	
 @endsection
 
