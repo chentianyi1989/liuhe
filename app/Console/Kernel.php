e5017b7b197bf3af3acca539379a02bedacecf9b
448987985@qq.com <448987985@qq.com>
2017-12-16 15:19:19 +0800

    1

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
+        \App\Console\Commands\LiuHe::class
     ];
 
     /**
@@ -23,6 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('liuhe:start_result')
+        ->everyFiveMinutes();
     }
 
     /**
